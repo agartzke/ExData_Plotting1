8e059a4d443b3dd325ecfec04d39287f05d08179
agartzke <agartzke@lucidwave.com>
2014-10-12 14:32:46 -0500

    Plot1.R cleanup and all Plot 2 code and output.

--- plot2.R
+++ plot2.R
@@ -3,8 +3,6 @@ library(data.table)
 # setup date formatting
 setClass("myDate")
 setAs("character", "myDate", function(from) as.Date(from, format="%d/%m/%Y"))
-#setClass("myTime")
-#setAs("character", "myTime", function(from) strptime(from, format="%H:%M:%S"))
 
 # grab full file, separated by ';' and interpret '?' as NAs
 rawDF <- read.table("./data/household_power_consumption.txt", sep = ";", header=TRUE,  na.strings="?", colClasses=c(Date="myDate"))
@@ -12,9 +10,10 @@ rawDT <- data.table(rawDF)
 
 # subset the data to this date range: 2007-02-01 to 2007-02-02 - 2 day period
 subDT <- subset(rawDT, (Date == as.Date("2007-02-01") | Date == as.Date("2007-02-02")))
+subDT[, DateTime:= as.POSIXct(paste(subDT$Date, subDT$Time), format="%Y-%m-%d %H:%M:%S")]
 
-hist(subDT$Global_active_power, col="red", xlab="Global Active Power (kilowatts)", main="Global Active Power")
+with(subDT, plot(DateTime, Global_active_power, type="l", ylab="Global Active Power (kilowatts)"))
 
 # dump to png
-dev.copy(png, file = "plot1.png")
+dev.copy(png, file = "plot2.png")
 dev.off()
