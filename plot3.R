16081d6b9fb94347f4dfa54fc1258d6006181b6b
agartzke <agartzke@lucidwave.com>
2014-10-12 14:58:23 -0500

    plot 3 code and png

--- plot3.R
+++ plot3.R
@@ -17,7 +17,9 @@ with(subDT, points(DateTime, Sub_metering_2, type="l", col="red"))
 with(subDT, points(DateTime, Sub_metering_3, type="l", col="blue"))
 
 # legend
-legend("topright", lty=c(1,1,1), col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+legend("topright", lty=c(1,1,1), col = c("black", "red", "blue"), 
+       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
+       cex=0.7, pt.cex = 1)
 
 # dump to png
 dev.copy(png, file = "plot3.png")
